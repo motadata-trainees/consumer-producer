53a61ce1b5bf281196dcf5403f595a1cbafe0528
Keya1999 <61039827+Keya1999@users.noreply.github.com>
2020-04-01 11:40:37 +0530

    Add files via upload

---
+++ M_producer_M_consumer(python)/main.py
@@ -0,0 +1,43 @@
+from queue import Queue
+from threading import Thread
+from random import randrange
+
+queue = Queue(10)
+
+class Consumer(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        #while True:
+            num = self.queue.get()
+            self.queue.task_done()
+            print('consumer get number: ' + str(num))
+
+
+
+class Producer(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        #while True:
+            num = randrange(1, 10)
+            self.queue.put(num)
+
+            print ('producer produce number : ' + str(num))
+
+
+def main():
+    for i in range(5):
+        p = Producer(queue).start()
+        c = Consumer(queue).start()
+
+
+    queue.join()
+
+
+if __name__ == "__main__":
+    main()
